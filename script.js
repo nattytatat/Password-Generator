386956e8ee493f9b963c5a9a31be1a2812bc332f
Natalie <natalie@bigorangemedia.co.uk>
2023-01-06 20:02:30 +0000

    add starter files

---
+++ script.js
@@ -0,0 +1,131 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+var userSelection = '';
+
+
+// Function for getting a random element from an array
+// function getRandom(arr) {
+
+//   var numberMixer = Math.floor(Math.random() * specialCharacters.length);
+//   if (charLower) += 
+  
+
+// }
+
+// Function to generate password with user input
+function generatePassword() {
+  
+      var charLength = prompt("Enter the number of characters you'd like in your password");
+      if (charLength < 10 || charLength > 64 ) {
+          alert("You must select a number between 10 and 64");
+        generatePassword();
+      }
+      
+      var charLower = confirm("Would you like lower case characters?");
+      var charUpper = confirm("Would you like Uppercase characters?");
+      var charNum = confirm("Would you like numbers?");
+      var charSpec = confirm("Would you like special characters?");
+   
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
