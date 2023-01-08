b5ee08cdb4315ce914e7dc2bd9dbab393b6f8c48
Natalie <natalie@bigorangemedia.co.uk>
2023-01-08 12:18:42 +0000

    start user prompt, validating input type and number

--- script.js
+++ script.js
@@ -89,32 +89,66 @@ var upperCasedCharacters = [
 ];
 
 var userSelection = '';
+var charLength = 0;
+var randomArray = [];
 
 
 // Function for getting a random element from an array
-// function getRandom(arr) {
+function getRandom(arr) {
 
-//   var numberMixer = Math.floor(Math.random() * specialCharacters.length);
-//   if (charLower) += 
-  
+ 
 
-// }
+  // Push the chosen arrays from the prompt to an empty array
+  if (charLower) randomArray.push(lowerCasedCharacters);
+  if (charUpper) randomArray.push(upperCasedCharacters);
+  if (charNum) randomArray.push(numericCharacters);
+  if (charSpec) randomArray.push(specialCharacters);
+
+  // randomise all the new objects in the array
+  for (var i = 0; i < randomArray.length; i++){
+    var randomNum = Math.floor(Math.random() * randomArray.length);
+      userSelection += randomArray[randomNum];
+  }
+
+  return password;
+
+}
 
 // Function to generate password with user input
 function generatePassword() {
   
-      var charLength = prompt("Enter the number of characters you'd like in your password");
-      if (charLength < 10 || charLength > 64 ) {
-          alert("You must select a number between 10 and 64");
-        generatePassword();
+      charLength = prompt("Enter the number of characters you'd like in your password");
+
+      //if the user selects cancel, write this result.
+      if (charLength === null) {
+        alert("You have chosen not to select a password.");
+        return;
+      //validate the length of the password, and check input is integer
+      } else if (!Number.isInteger(charLength) || charLength < 10 || charLength > 64 ) {
+        alert("Password must be a number between 10 and 64");
+        return;
+
+      } else {
+        promptReturn();
       }
+
+      //Character confirm Questions
+
+function promptReturn() {
+  var charLower = confirm("Would you like lower case characters?");
+  var charUpper = confirm("Would you like Uppercase characters?");
+  var charNum = confirm("Would you like numbers?");
+  var charSpec = confirm("Would you like special characters?");
+  
+  }
+
+    }
+  
+  
+
+
       
-      var charLower = confirm("Would you like lower case characters?");
-      var charUpper = confirm("Would you like Uppercase characters?");
-      var charNum = confirm("Would you like numbers?");
-      var charSpec = confirm("Would you like special characters?");
-   
-}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
